d34dd1362c0205c16af65e3589e4d77623d0151b
James Montalvo <edwin.j.montalvo@nasa.gov>
2015-05-13 15:21:03 -0500

    animate pagescore expansion

--- modules/pagescores/pagescores.js
+++ modules/pagescores/pagescores.js
@@ -8,6 +8,49 @@
 			$("#ext-watchanalytics-pagescores-template")[0].innerHTML
 		);
 
+		var pageScoresTimeoutId;
+
+		$("#ext-watchanalytics-pagescores").hover(
+			function() {
+
+				clearTimeout( pageScoresTimeoutId );
+
+				$(this).find('.ext-watchanalytics-pagescores-right').css({
+					"border-radius": "0 4px 4px 0"
+				});
+
+				$(this).find('.ext-watchanalytics-pagescores-left')
+					.css({
+						"border-radius": "4px 0 0 4px"
+					})
+					.animate(
+						{ width: "show" },
+						50,
+						"linear"
+					);
+
+			},
+			function() {
+				var self = this;
+
+				pageScoresTimeoutId = setTimeout(
+					function() {
+						$(self).find('.ext-watchanalytics-pagescores-left')
+							.animate(
+								{ width: "hide" },
+								50,
+								"linear"
+							);
+
+						$(self).find('.ext-watchanalytics-pagescores-right').css({
+							"border-radius": "4px"
+						});
+					},
+					2000 
+				);
+			}
+		);
+
 	});
 
 } )( jQuery, mediaWiki );
