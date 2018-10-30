05813ec83c7bd4a122a0e06208774b7a56569617
Kris Field <kristopher.field@gmail.com>
2018-10-29 21:37:27 -0500

    fixes query

--- specials/SpecialClearPendingReviews.php
+++ specials/SpecialClearPendingReviews.php
@@ -30,32 +30,45 @@ class SpecialClearPendingReviews extends SpecialPage {
 		$output->addHTML("</select>");
 		$output->addHTML('<button type="button" action="$action" method="post">Clear Pending Reviews</button>');
 
+    $currentTime = date("YmdHis");
+    $startTime = $currentTime-3000;
+    $category = 'Test';
 		$dbw = wfGetDB( DB_MASTER );
 		$res = $dbw->select(
-      array(
-        'tables' => array(
+        array(
           'w' => 'watchlist',
           'p' => 'page',
           'c' => 'categorylinks',
         ),
-        'fields' => array(
-          'w.*',
+        array(
+          'w.*'
         ),
-        'join_conds' => array(
+        "c.cl_to='$category' AND w.wl_notificationtimestamp IS NOT NULL AND w.wl_notificationtimestamp < $currentTime AND w.wl_notificationtimestamp > $startTime",
+        array(
           'p' => array(
             'LEFT JOIN', 'w.wl_title=p.page_title'
           ),
           'c' => array(
-            'LEFT JOIN', 'c.cl_from = p.page_id'
-          ),
-        ),
-        'conds' => "c.cl_to=Test"
-      )
-    );
+            'LEFT JOIN', 'c.cl_from=p.page_id'
+          )
+        )
+      );
 
+    $output->addHTML("<table>");
+    $output->addHTML("<tr><th>ID</th><th>User</th><th>Namespace</th><th>Title</th><th>Notification TimeStamp</th></tr>");
     foreach ($res as $value) {
-      $output->addHTML($value);
+      $output->addHTML("<tr>");
+      $output->addHTML("<td>".$value->wl_id."</td>");
+      $output->addHTML("<td>".$value->wl_user."</td>");
+      $output->addHTML("<td>".$value->wl_namespace."</td>");
+      $output->addHTML("<td>".$value->wl_title."</td>");
+      $output->addHTML("<td>".$value->wl_notificationtimestamp."</td>");
+      $output->addHTML("</tr>");
     }
+    $output->addHTML("</table>");
+
+    $output->addHTML("<b>Start time:</b>".$startTime);
+    $output->addHTML("<b>Start time:</b>".$currentTime);
   }
   // static public function removePendingReviewsByCategory($start, $category) {
   //   return array(
