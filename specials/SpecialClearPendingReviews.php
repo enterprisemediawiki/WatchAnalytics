31fa22c4d1965f28ce5e50cc464019d2fc1ce91b
Kris Field <kristopher.field@gmail.com>
2018-10-30 22:31:52 -0500

    adds search form

--- specials/SpecialClearPendingReviews.php
+++ specials/SpecialClearPendingReviews.php
@@ -1,95 +1,104 @@
+
 <?php
 /**
- * MezaAdmin SpecialPage
- *
- * @file
- * @ingroup Extensions
- */
+* ClearPendingReviews SpecialPage
+*
+* @file
+* @ingroup Extensions
+*/
 
-class SpecialClearPendingReviews extends SpecialPage {
+class SpecialClearPendingReviews extends FormSpecialPage {
 	public function __construct() {
 		parent::__construct( 'ClearPendingReviews' );
 	}
 
-	public function execute( $par ) {
+	protected function getFormFields() {
+		return [
+			'category' => [
+				'label-message' => 'clearpendingreview-category',
+				'type' => 'text',
+				'required' => 'false',
+			],
+			'start' => [
+				'label-message' => 'clearpendingreview-start-time',
+				'type' => 'text',
+				'required' => 'true',
+			],
+		];
+	}
+
+	/**
+	*TO DO
+	*Add field validation to protect
+	**No malicious inputs allowed
+	**Validate entered time is real time
+	**Verify category entered exists on wiki
+	**Allow for wild card pagename entry in lieu of category
+	*Get rid of warning "Cannot modify header information"
+	*Allow for end time or just use current time
+	*Display current time in ISO on the page for reference
+	*Show number of pages/categories that will be cleared before clearpendingreview
+	*only allow sysop to do this, add new userright
+	*/
+
+	/**
+	 * @param HTMLForm $form
+	 */
+	protected function alterForm( HTMLForm $form ) {
+		$form->setSubmitTextMsg( 'clearpendingreview-submit' );
+	}
+
+	/**
+	*TO DO
+	*/
+
+	/**
+ * @param array $data
+ * @return Status
+ */
+	public function onSubmit( array $data ) {
+		$currentTime = date("YmdHis");
+		$dbw = wfGetDB( DB_MASTER );
+		$category = preg_replace('/\s+/', '', $data['category']);
+		$start = preg_replace('/\s+/', '', $data['start']);
+
+		$res = $dbw->select(
+				array(
+					'w' => 'watchlist',
+					'p' => 'page',
+					'c' => 'categorylinks',
+				),
+				array(
+					'w.*'
+				),
+				"c.cl_to='$category' AND w.wl_notificationtimestamp IS NOT NULL AND w.wl_notificationtimestamp < $currentTime AND w.wl_notificationtimestamp > $start",
+				array(
+					'p' => array(
+						'LEFT JOIN', 'w.wl_title=p.page_title'
+					),
+					'c' => array(
+						'LEFT JOIN', 'c.cl_from=p.page_id'
+					)
+				)
+			);
 		$request = $this->getRequest();
 		$output = $this->getOutput();
 		$this->setHeaders();
+		$output->addHTML("<table>");
+		$output->addHTML("<tr><th>ID</th><th>User</th><th>Namespace</th><th>Title</th><th>Notification TimeStamp</th></tr>");
+		foreach ($res as $value) {
+			$output->addHTML("<tr>");
+			$output->addHTML("<td>".$value->wl_id."</td>");
+			$output->addHTML("<td>".$value->wl_user."</td>");
+			$output->addHTML("<td>".$value->wl_namespace."</td>");
+			$output->addHTML("<td>".$value->wl_title."</td>");
+			$output->addHTML("<td>".$value->wl_notificationtimestamp."</td>");
+			$output->addHTML("</tr>");
+		}
+		$output->addHTML("</table>");
 
-		$output->addHTML('<h2>Clear Pending Reviews</h2>');
-		$output->addHTML("<p>Use these drop downs to select the page category and time frame to clear pending reviews from.</p>");
-		$output->addHTML("<select>");
-		$output->addHTML('<option value="Test">Test</option>');
-  	$output->addHTML('<option value="Test2">Test2</option>');
-		$output->addHTML("</select>");
-		$output->addHTML("<select>");
-		$output->addHTML('<option value="0.5">30 minutes ago</option>');
-		$output->addHTML('<option value="1">1 hour ago</option>');
-  	$output->addHTML('<option value="2">2 hours ago</option>');
-		$output->addHTML('<option value="2">5 hours ago</option>');
-		$output->addHTML("</select>");
-		$output->addHTML('<button type="button" action="$action" method="post">Clear Pending Reviews</button>');
-
-    $currentTime = date("YmdHis");
-    $startTime = $currentTime-3000;
-    $category = 'Test';
-		$dbw = wfGetDB( DB_MASTER );
-		$res = $dbw->select(
-        array(
-          'w' => 'watchlist',
-          'p' => 'page',
-          'c' => 'categorylinks',
-        ),
-        array(
-          'w.*'
-        ),
-        "c.cl_to='$category' AND w.wl_notificationtimestamp IS NOT NULL AND w.wl_notificationtimestamp < $currentTime AND w.wl_notificationtimestamp > $startTime",
-        array(
-          'p' => array(
-            'LEFT JOIN', 'w.wl_title=p.page_title'
-          ),
-          'c' => array(
-            'LEFT JOIN', 'c.cl_from=p.page_id'
-          )
-        )
-      );
-
-    $output->addHTML("<table>");
-    $output->addHTML("<tr><th>ID</th><th>User</th><th>Namespace</th><th>Title</th><th>Notification TimeStamp</th></tr>");
-    foreach ($res as $value) {
-      $output->addHTML("<tr>");
-      $output->addHTML("<td>".$value->wl_id."</td>");
-      $output->addHTML("<td>".$value->wl_user."</td>");
-      $output->addHTML("<td>".$value->wl_namespace."</td>");
-      $output->addHTML("<td>".$value->wl_title."</td>");
-      $output->addHTML("<td>".$value->wl_notificationtimestamp."</td>");
-      $output->addHTML("</tr>");
-    }
-    $output->addHTML("</table>");
-
-    $output->addHTML("<b>Start time:</b>".$startTime);
-    $output->addHTML("<b>Start time:</b>".$currentTime);
-  }
-  // static public function removePendingReviewsByCategory($start, $category) {
-  //   return array(
-  //     'tables' => array(
-  //       'w' => 'watchlist',
-  //       'p' => 'page',
-  //       'c' => 'categorylinks',
-  //     ),
-  //     'fields' => array(
-  //       'w.*',
-  //     ),
-  //     'join_conds' => array(
-  //       'p' => array(
-  //         'JOIN', 'w.wl_title=p.page_title'
-  //       ),
-  //       'c' => array(
-  //         'LEFT OUTER JOIN', 'c.cl_from = p.page_id'
-  //       ),
-  //     ),
-  //     'conds' => "c.cl_to => $category AND w.wl_notificationtimestamp < $now AND w.wl_notificationtimestamp > $start",
-  //   );
-  // }
-
+		$output->addHTML("<b>Start time:</b>".$start."<br>");
+		$output->addHTML("<b>End time:</b>".$currentTime);
+		return Status::newGood();
+	}
 }
