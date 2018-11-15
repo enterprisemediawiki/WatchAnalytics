690eff1af939a82c7ec1c60557f6454e8584d72b
Kris Field <kristopher.field@gmail.com>
2018-11-14 19:22:31 -0600

    use keydown event

--- modules/clearpendingreviews/ext.watchanalytics.clearpendingreviews.js
+++ modules/clearpendingreviews/ext.watchanalytics.clearpendingreviews.js
@@ -1,6 +1,6 @@
 $( function ()  {
 	$(document).ready( function () {
-		$('.oo-ui-inputWidget-input').change(
+		$('.oo-ui-inputWidget-input').keydown(
 			function() {
 				$(".oo-ui-buttonElement-button[name=clearpages]").css({ "value": "Preview", "name":"Preview", "background-color": "#36c", "border-color": "#36c"  });
 				$(".oo-ui-buttonElement-button[name=clearpages]").attr( "name", "preview");
